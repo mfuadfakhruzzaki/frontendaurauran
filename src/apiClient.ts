bdc278f2b0241347226aeafb0cc8c7f56ca8f9cd
Frmngh <128209700+Frmngh@users.noreply.github.com>
2024-11-25 21:22:45 +0700

    Fitur homepage, tapi detailnya belom ada

--- src/apiClient.ts
+++
@@ -1,9 +0,0 @@
-// src/apiClient.ts
-import axios from "axios";
-
-const apiClient = axios.create({
-  baseURL: "https://api.zacht.tech",
-  headers: { "Content-Type": "application/json" },
-});
-
-export default apiClient;
